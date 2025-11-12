4abc17d6a10a9ccfe37cbac4787d50f8a84e5a34
Kory Carrera <koritocarrera@gmail.com>
2025-11-12 16:57:51 -0500

    Reestructuracion del proyecto a estructura MVC

--- modules/admin/assets/models_call.js
+++
@@ -1,7 +0,0 @@
-async function cargarComponente(idElemento, rutaArchivo) {
-	const respuesta = await
-	fetch(rutaArchivo);
-	const html = await respuesta.text();
-	
-	document.getElementById(idElemento).innerHTML = html;
-	}
