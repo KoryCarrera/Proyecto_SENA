1a1dd78f9a1ebeb9a5c46f7d8404d45784dd0663
KoryCarrera <koritocarrera@gmail.com>
2026-02-27 20:14:18 -0500

    Se integró la libreria PHPMailer reemplazando resend y se mejoró el uso de dotenv

--- Public/index.php
+++ Public/index.php
@@ -3,6 +3,12 @@
 //Llamamos el autoloader de vendor para inicializar altorouter
 require_once __DIR__ . '/../vendor/autoload.php';
 
+//Insertamos la libreria para las env
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 //Cargamos el mapa de rutas (El archivo router.php)
 $router = require_once __DIR__ . '/../app/router.php';
 
@@ -16,15 +22,14 @@ if ($match) {
   //Si hay match cargamos el archivo correspondiente
 
   //Construimos la ruta real
-  $destino = __DIR__. '/../app/' . $match['target'];
+  $destino = __DIR__ . '/../app/' . $match['target'];
 
   //validamos existencia del archivo
   if (file_exists($destino)) {
-  
-  require_once $destino;
-  
-    } else {
-   
+
+    require_once $destino;
+  } else {
+
     //Agregamos un manejo de errores interno
     header($_SERVER['SERVER_PROTOCOL'] . '500 Internar Error!');
     echo "Error: el archivo <b>{$match['target']}</b> No existe";
@@ -34,14 +39,10 @@ if ($match) {
     header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type, Authorization');
   }
+} else {
 
-  } else {
-    
-    //Agregamos el manejo de error de Not Found (404)
-    header($_SERVER['SERVER_PROTOCOL'] . '404 Not Found!');
+  //Agregamos el manejo de error de Not Found (404)
+  header($_SERVER['SERVER_PROTOCOL'] . '404 Not Found!');
 
-    echo "<h1>¡Ruta no encontrada!<h1>";
-
-  }
-
-?>
+  echo "<h1>¡Ruta no encontrada!<h1>";
+}
