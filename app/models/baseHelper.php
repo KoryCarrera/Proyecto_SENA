c60de96497f6d20822547cc1acdb0913cdd8a3be
KoryCarrera <koritocarrera@gmail.com>
2026-03-05 09:50:18 -0500

    Se crea una clase helper para consultas e inserciones genericas a la bd

---
+++ app/models/baseHelper.php
@@ -0,0 +1,147 @@
+<?php
+class baseHelper {
+
+public function __construct(private  $conexion) 
+{}
+    
+
+    public function consultObjectHelper($sp) {
+        $stmt = $this->conexion->prepare("CALL $sp");
+
+        try {
+            $stmt->execute();
+            $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $stmt->closeCursor();
+
+            if ($data && is_array($data)){
+
+                return $data;
+            } else {
+                throw new Exception("No se han podido obtener datos");
+            }
+
+        } catch (PDOException $e) {
+            error_log('Ha ocurrido un error sql: ' . $e->getMessage());
+
+            throw new Exception("Ha ocurrido un error, revisa los logs del servidor");
+        }
+    }
+
+    public function consultSimpleHelper($sp){
+        $stmt = $this->conexion->prepare("CALL $sp");
+
+        try{
+
+            $stmt->execute();
+            $data = $stmt->fetch(PDO::FETCH_ASSOC);
+            $stmt->closeCursor();
+
+            if ($data && is_array($data)){
+                return $data;
+            } else {
+                throw new Exception("No se ha podido obtener datos de la consulta");
+            }
+
+        } catch (PDOException $e) {
+            error_log("Ha ocurrido un error sql: " . $e->getMessage());
+
+            throw new Exception("Ha ocurrido un error, revisa los logs del servidor");
+        }
+    }
+
+    public function consultSimpleWithParams($sp, $params){
+        
+        if (!$sp || !$params) {
+            throw new Exception("Datos ingresados al metodo vacios");
+        }
+
+        if (!is_array($params) || !is_string($sp)){
+            throw new Exception("Datos ingresados al metodo invalidos, por favor verifica");
+        };
+
+        $stmt = $this->conexion->prepare("CALL $sp");
+        
+        $i = 1;
+        foreach ($params as $param => $type) {
+            $stmt->bindParam($i, $param, $type);
+        };
+
+        try{
+
+            $stmt->execute();
+            $data = $stmt->fetch(PDO::FETCH_ASSOC);
+            $stmt->closeCursor();
+
+            if ($data && is_array($data)){
+                return $data;
+            } else {
+                throw new Exception("No se ha podido obtener datos de la consulta con parametros");
+            }
+
+        } catch (PDOException $e) {
+            error_log("Ha ocurrido un error sql: " . $e->getMessage());
+
+            throw new Exception("Ha ocurrido un error, revisa los logs del servidor");
+        };
+    }
+
+    public function consultObjectWithParams($sp, $params){
+        
+        if (!$sp || !$params) {
+            throw new Exception("Datos ingresados al metodo vacios");
+        }
+
+        if (!is_array($params) || !is_string($sp)){
+            throw new Exception("Datos ingresados al metodo invalidos, por favor verifica");
+        };
+
+        $stmt = $this->conexion->prepare("CALL $sp");
+        
+        $i = 1;
+        foreach ($params as $param => $type) {
+            $stmt->bindParam($i, $param, $type);
+        };
+
+        try{
+            
+            $stmt->execute();
+            $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $stmt->closeCursor();
+
+            if ($data && is_array($data)){
+                return $data;
+            } else {
+                throw new Exception("No se ha podido obtener datos de la consulta con parametros");
+            }
+
+        } catch (PDOException $e) {
+            error_log("Ha ocurrido un error sql: " . $e->getMessage());
+
+            throw new Exception("Ha ocurrido un error, revisa los logs del servidor");
+        };
+    }
+
+    public function insertOrUpdateData($sp, $data){
+        $stmt = $this->conexion->prepare("CALL $sp");
+
+        $i = 1;
+        foreach ($data as $param => $type) {
+            $stmt->bindParam($i, $param, $type);
+        };
+
+        try {
+            $stmt->execute();
+            $dataR = $stmt->fetch(PDO::FETCH_ASSOC);
+            $stmt->closeCursor();
+
+            if($dataR && is_array($dataR)){
+                return $dataR;
+            } else {
+                throw new Exception("No se ha podido recuperar los datos ingresados");
+            }
+        } catch (PDOException $e) {
+            error_log("Ha ocurrido un erro sql: ". $e->getMessage());
+            return false;
+        }
+    }
+};
