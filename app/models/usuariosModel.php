7778fb8c460d92f211ced161c75112b78fc4dd5e
KoryCarrera <koritocarrera@gmail.com>
2026-03-10 10:12:10 -0500

    Kyca refactor poo (#181)
    * Se crea una clase helper para consultas e inserciones genericas a la bd
    
    * Se integran validaciones de argumentos de los metodos
    
    * Se crea modelo con POO de cambiar estado usuario
    
    * Listar casos refac (comi y admin)
    
    * Se crea modelo de configuracion de perfil de usuario en POO con validaciones
    
    * Corrección de logica de hasheo de contrasña
    
    * Se hace modelo de gestionar y crear usuario con validaciones y topkens aleatorios

--- app/models/usuariosModel.php
+++ app/models/usuariosModel.php
@@ -1,65 +1,138 @@
  <?php
-class UsuariosModdel extends baseHelper
-{
-    public function cambiarEstadoUsuario($documentoFind, $nuevoEstado, $documentoSession)
+    class UsuariosModdel extends baseHelper
     {
+        public function cambiarEstadoUsuario($documentoFind, $nuevoEstado, $documentoSession)
+        {
 
-        if($documentoFind == $documentoSession){
-            throw new Exception('¡Por seguridad no se puede desactivar a uno mismo!');
-        };
+            if ($documentoFind == $documentoSession) {
+                throw new Exception('¡Por seguridad no se puede desactivar a uno mismo!');
+            };
 
-        try {
+            try {
 
+                $data = [
+                    ['value' => $documentoFind, 'type' => PDO::PARAM_STR],
+                    ['value' => $nuevoEstado, 'type' => PDO::PARAM_INT],
+                ];
+
+                $cambioEstado = parent::insertOrUpdateData('sp_cambiar_estado_usuario(?, ?)', $data);
+
+                return true;
+            } catch (Exception $e) {
+
+                error_log('Ha ocurrido un error SQL a la hora de cambiar estado usuario: ' . $e->getMessage());
+
+                throw new Exception($e->getMessage());
+            }
+        }
+
+        public function configuracionPerfilUsuario($documento, $oldPass, $nombre = null, $apellido = null, $correo = null, $tlf = null, $newPass = null)
+        {
             $data = [
-                ['value' => $documentoFind, 'type' => PDO::PARAM_STR],
-                ['value' => $nuevoEstado, 'type' => PDO::PARAM_INT],
+                ['value' => $documento, 'type' => PDO::PARAM_STR]
             ];
 
-            $cambioEstado = parent::insertOrUpdateData('sp_cambiar_estado_usuario(?, ?)', $data);
+            $userVerify = parent::consultSimpleWithParams('sp_login_usuario(?)', $data);
 
-            return true;
+            if (!$userVerify) {
+                throw new Exception('¡No se ha podido encontrar el usuario logueado!');
+            }
 
-        } catch (Exception $e) {
+            if (!password_verify($oldPass, $userVerify['contraseña'])) {
+                throw new Exception('¡Contraseña invalida, trata de nuevo!');
+            }
 
-            error_log('Ha ocurrido un error SQL a la hora de cambiar estado usuario: ' . $e->getMessage());
+            $passToSave = $newPass ? password_hash($newPass, PASSWORD_BCRYPT) : null;
 
-            throw new Exception($e->getMessage());
+            $newData = [
+                ['value' => $documento, 'type' => PDO::PARAM_STR],
+                ['value' => $nombre, 'type' => PDO::PARAM_STR],
+                ['value' => $apellido, 'type' => PDO::PARAM_STR],
+                ['value' => $correo, 'type' => PDO::PARAM_STR],
+                ['value' => $passToSave, 'type' => PDO::PARAM_STR],
+                ['value' => $tlf, 'type' => PDO::PARAM_STR],
+            ];
+
+            try {
+
+                parent::insertOrUpdateData('sp_configurar_usuario(?, ?, ?, ?, ?, ?)', $newData);
+            } catch (Exception $e) {
+                error_log('Error al configurar perfil de usuario: ' . $e->getMessage());
+                throw new Exception($e);
+            }
+        }
+
+        public function crearUsuario($documento, $nombre, $apellido, $email, $numero, $id_rol)
+        {
+
+            $token = bin2hex(random_bytes(5));
+
+            $contraseñaHash = password_hash($token, PASSWORD_BCRYPT);
+
+            $data = [
+                ['value' => $documento, 'type' => PDO::PARAM_STR],
+                ['value' => $nombre, 'type' => PDO::PARAM_STR],
+                ['value' => $apellido, 'type' => PDO::PARAM_STR],
+                ['value' => $email, 'type' => PDO::PARAM_STR],
+                ['value' => $numero, 'type' => PDO::PARAM_STR],
+                ['value' => $id_rol, 'type' => PDO::PARAM_INT],
+                ['value' => $contraseñaHash, 'type' => PDO::PARAM_STR],
+            ];
+
+            try {
+                parent::insertOrUpdateData('sp_registrar_usuario(?, ?, ?, ?, ?, ?, ?)', $data);
+
+                return $token;
+            } catch (Exception $e) {
+                error_log('Error al crear usuario: ' . $e->getMessage());
+                throw new Exception($e);
+            }
+        }
+
+        public function gestionarUsuario(
+            $documento,
+            $nombre = null,
+            $apellido = null,
+            $email = null,
+            $numero = null,
+            $id_rol = null,
+            $refreshPass = false
+        ) {
+
+            $dataFUser = [
+                ['value' => $documento, 'type' => PDO::PARAM_STR]
+            ];
+
+            $findUSer = parent::consultSimpleWithParams('sp_traer_usuario(?)', $dataFUser);
+
+            if (!$findUSer) {
+                throw new Exception('¡No se ha podido encontrar el usuario!');
+            };
+
+            $passToSave = null;
+
+            if ($refreshPass !== false) {
+                $token = bin2hex(random_bytes(5));
+                $passToSave = password_hash($token, PASSWORD_BCRYPT);
+            };
+
+            $dataInsert = [
+                ['value' => $documento, 'type' => PDO::PARAM_STR],
+                ['value' => $nombre, 'type' => PDO::PARAM_STR],
+                ['value' => $apellido, 'type' => PDO::PARAM_STR],
+                ['value' => $email, 'type' => PDO::PARAM_STR],
+                ['value' => $numero, 'type' => PDO::PARAM_STR],
+                ['value' => $id_rol, 'type' => PDO::PARAM_INT],
+                ['value' => $passToSave, 'type' => PDO::PARAM_STR],
+            ];
+
+            try{
+
+            parent::insertOrUpdateData('sp_gestionar_usuario(?, ?, ?, ?, ?, ?, ?)', $dataInsert);
+            
+            } catch(Exception $e){
+                error_log('Error al gestionar usuario: ' . $e->getMessage());
+                throw new Exception($e);
+            }
         }
     }
-
-    public function configuracionPerfilUsuario($documento, $oldPass, $nombre = null, $apellido = null, $correo = null, $tlf = null, $newPass = null)
-    {
-        $data = [
-            ['value' => $documento, 'type' => PDO::PARAM_STR]
-        ];
-
-        $userVerify = parent::consultSimpleWithParams('sp_login_usuario(?)', $data);
-
-        if (!$userVerify) {
-            throw new Exception('¡No se ha podido encontrar el usuario logueado!');
-        }
-
-        if (!password_verify($oldPass, $userVerify['contraseña'])) {
-            throw new Exception('¡Contraseña invalida, trata de nuevo!');
-        }
-
-        $passToSave = $newPass ? password_hash($newPass, PASSWORD_BCRYPT) : null;
-
-        $newData = [
-            ['value' => $documento, 'type' => PDO::PARAM_STR],
-            ['value' => $nombre, 'type' => PDO::PARAM_STR],
-            ['value' => $apellido, 'type' => PDO::PARAM_STR],
-            ['value' => $correo, 'type' => PDO::PARAM_STR],
-            ['value' => $passToSave, 'type' => PDO::PARAM_STR],
-            ['value' => $tlf, 'type' => PDO::PARAM_STR],
-        ];
-
-        try {
-
-            parent::insertOrUpdateData('sp_configurar_usuario(?, ?, ?, ?, ?, ?)', $newData);
-        } catch (Exception $e) {
-            error_log('Error al configurar perfil de usuario: ' . $e->getMessage());
-            throw new Exception($e);
-        }
-    }
-}
