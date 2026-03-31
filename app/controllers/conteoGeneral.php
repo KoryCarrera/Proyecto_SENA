10e8d5e1e09e2cfbe2f38b2c51f4bbda3a043522
JuanGalvis <correaljuan171@gmail.com>
2026-03-31 15:39:59 -0500

    documentacion de los controladores conteogeneral,configuracionUsuario,cambiaarestadoUsuario y 2fa

--- app/controllers/conteoGeneral.php
+++ app/controllers/conteoGeneral.php
@@ -2,10 +2,18 @@
 
 header('Content-Type: application/json');
 
+//se define el formato de respuesta y peticion de con la que trabajamos (json)
+
 require_once __DIR__ . "/../config/conexion.php";
 require_once __DIR__ . "/../models/baseHelper.php";
 
+//llamamos las dependencias necesarias para el funcionamiento del controlador
+
+//se crea una instancia de la clase baseHelper
+
 $helper = new baseHelper($pdo);
+
+//se valida que el metodo sea GET 
 
 if($_SERVER['REQUEST_METHOD'] !== 'GET'){
     echo json_encode([
@@ -14,7 +22,12 @@ if($_SERVER['REQUEST_METHOD'] !== 'GET'){
     ]);
     exit;
 }
+
+//se consulta el total de casos
+
 $conteo = $helper->consultSimpleHelper('sp_resumen_casos_global');
+
+//se valida que no sea nulo el conteo de casos
 
 if (!$conteo) {
     echo json_encode([
@@ -23,6 +36,8 @@ if (!$conteo) {
     ]);
     exit;
 };
+
+//se crea un aray con la separacion por datos para su posterior uso de manera sencilla
 
 $parametros = [
     'total' => $conteo['total_casos'] ?? 0,
